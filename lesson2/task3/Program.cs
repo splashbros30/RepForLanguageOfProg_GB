87a16992ab9ed63fb08f251fba2b162ca36dbc3d
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-01-13 15:55:55 +0300

    добавил незакоммиченное задание 3 из лекции 2

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//int N = 10;
+int[] arr = { 2, 5, 7, 4, 65, 135, 546, 43, 32, 13 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if ((arr[i] % 2) == 0)
+    {
+        Console.Write($"{arr[i]} ");
+        i = i + 1;
+    }
+    else
+    {
+        i = i + 1;
+    }
+}
