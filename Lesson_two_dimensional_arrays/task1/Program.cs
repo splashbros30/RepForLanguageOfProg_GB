21d47f50c4630e9323fc9b1a13316df79ae54b63
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-01-23 20:11:58 +0300

    Добавил урок и первое задание к нему

---
+++ Lesson_two_dimensional_arrays/task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// int[,] matrix = new int[3, 5];
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i, j] = new Random().Next(1, 11);
+//     }
+// }
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($"{matrix[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+int[,] CreateMatrix(int row_count, int colums_count)
+{
+    int[,] matrix = new int[row_count, colums_count];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void show_matrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] Matrix = CreateMatrix(4, 6);
+show_matrix(Matrix);
