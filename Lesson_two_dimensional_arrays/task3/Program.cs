6cd41b5ce06c97abeab758b7ef9a092dda519920
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-01-29 13:42:41 +0300

    Добавил решение 3 задачи из лекции

---
+++ Lesson_two_dimensional_arrays/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №3
+// Лекция 4. Двумерные массивы
+// Считать с консоли строку, состоящую из 
+// цифр и латинских букв.
+// Сформировать новую строку, состоящую из 
+// букв исходной строки.
+
+string get_letters_from_string(string str)
+{
+  string let = "";
+  foreach (char e in str)
+  {
+    if ((char.IsLetter(e) == true))
+    {
+      let = let + e;
+    }
+  }
+  return let;
+}
+
+Console.WriteLine("Введите строку: ");
+string a = Console.ReadLine()!;
+string res = get_letters_from_string(a);
+Console.WriteLine("Только буквы в ведённой строке: ");
+Console.WriteLine(res);
